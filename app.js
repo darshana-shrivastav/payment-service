a03ccd94b899267618fbc31b98878cb20e1e9ebe
Darshana Shrivastava <darshana.s465@gmail.com>
2024-03-20 16:07:13 +0530

    Initial Setup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Define a route for the homepage
 app.get('/', (req, res) => {
